389de362055528ca1c8a61c8d6f505d10bf7d219
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-01-29 12:27:23 +0300

    .

--- robot_builder.py
+++ robot_builder.py
@@ -4,7 +4,7 @@ def rectangle(horizontal, vertical, color):
     t.pendown()
     t.pensize(1)
     t.color(color)
-    t.begin_fil()
+    t.begin_fill()
     for counter in range(1, 3):
         t.forward(horizontal)
         t.right(90)
